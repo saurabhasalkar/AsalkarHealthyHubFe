f87ae85030572d18ab552f6d3617ab13a3463ec3
Vaibhav Jadhav <vaibhav.jadhav0596@gmail.com>
2024-12-09 22:39:01 +0530

    Setup

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import AuthLayout from "./components/auth/layout";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+
 function App() {
-  return <>Hello world</>;
+  return (
+    <div className="flex flex-col overflow-hidden bg-white">
+      <Routes>
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
