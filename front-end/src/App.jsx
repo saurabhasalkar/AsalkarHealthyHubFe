e7cfcbff658656e0ae0ed51c133a0a5e186a64b3
Vaibhav Jadhav <vaibhav.jadhav0596@gmail.com>
2024-12-10 18:26:57 +0530

    Admin routes setup

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,6 +2,10 @@ import { Route, Routes } from "react-router-dom";
 import AuthLayout from "./components/auth/layout";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
+import AdminLayout from "./components/admin/Layout";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import AdminOrders from "./pages/admin/AdminOrders";
+import AdminProducts from "./pages/admin/AdminProducts";
 
 function App() {
   return (
@@ -10,6 +14,11 @@ function App() {
         <Route path="/auth" element={<AuthLayout />}>
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
+        </Route>
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="orders" element={<AdminOrders />} />
+          <Route path="products" element={<AdminProducts />} />
         </Route>
       </Routes>
     </div>
--- front-end/src/components/admin/Layout.jsx
+++ front-end/src/components/admin/Layout.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import AdminSidebar from "./AdminSidebar";
+import AdminHeader from "./AdminHeader";
 
 const AdminLayout = () => {
-  return <div> AdminLayout</div>;
+  return (
+    <div className="flex min-h-screen w-full">
+      <AdminSidebar />
+      <div className="flex flex-1 flex-col">
+        <AdminHeader />
+        <main className="flex-1 flex-col flex bg-muted/40 p-4 md:p-6">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default AdminLayout;
